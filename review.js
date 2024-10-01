c63c512e049d66c3b8f174a1b329a5005b0bbd6e
Jose Perez Jr <zenlaws02@gmail.com>
2024-10-02 03:45:38 +0800

    feat: reviewing selecting html elements and assigning to variables

--- review.js
+++ review.js
@@ -6,4 +6,13 @@ const fullname = firstname + ' ' + lastname;
 
 // selecting an html element
 const greeting = document.querySelector('#greeting');
+const berry_field = document.querySelector('#berry-field');
+const berry_btn = document.querySelector('#berry-btn');
+const msg = document.querySelector('#msg');
+
+// marquee
 greeting.textContent = `Hello, ${fullname}!`;
+
+berry_btn.addEventListener('click', function () {
+  greeting.textContent = 'Bitlog';
+});
