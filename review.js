d095b846953f8490029ee9b58b5b39d4f5f70ab9
Jose Perez Jr <zenlaws02@gmail.com>
2024-10-02 04:20:02 +0800

    feat: reviewing switch case, math.random, and const now = new Date()

--- review.js
+++ review.js
@@ -1,18 +1,51 @@
-const firstname = 'adfinem';
-const lastname = 'rising';
-const fullname = firstname + ' ' + lastname;
-
-// console.log(`good day, ${fullname}!`);
-
 // selecting an html element
 const greeting = document.querySelector('#greeting');
 const berry_field = document.querySelector('#berry-field');
 const berry_btn = document.querySelector('#berry-btn');
 const msg = document.querySelector('#msg');
 
-// marquee
-greeting.textContent = `Hello, ${fullname}!`;
+// const firstname = 'adfinem';
+// const lastname = 'rising';
+// const fullname = firstname + ' ' + lastname;
+// console.log(fullname);
+
+// GET current date and time today:
+const now = new Date(); // Get the current date and time
+const options = {
+  year: 'numeric',
+  month: 'long',
+  day: 'numeric',
+  hour: '2-digit',
+  minute: '2-digit',
+  // second: '2-digit',
+  hour12: true, // Change to true for 12-hour format
+};
+const formattedDateTime = now.toLocaleString('en-US', options);
+
+// default state of marquee:
+greeting.textContent = formattedDateTime;
 
 berry_btn.addEventListener('click', function () {
-  greeting.textContent = 'Bitlog';
+  const user_input = berry_field.value;
+  const randomNumber = Math.floor(Math.random() * 3) + 1;
+
+  switch (randomNumber) {
+    case 1:
+      greeting.textContent = 'jabagay';
+      break;
+    case 2:
+      greeting.textContent = 'bitlog';
+      break;
+    case 3:
+      if (user_input === '') {
+        return (greeting.textContent = 'Error: huh? did you enter your name??');
+      }
+      greeting.textContent = `Hello, ${user_input}!`;
+      break;
+    default:
+      greeting.textContent =
+        'Error: whoops! didnt see that coming.. some unknown error occured.';
+  }
 });
+
+const day = 3;
