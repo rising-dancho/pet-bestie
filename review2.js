7393f716e8b0e80ee23524ac4fc48aa75e412131
Jose Perez Jr <zenlaws02@gmail.com>
2024-10-04 23:27:48 +0800

    feat: reviewing object that has a function in it

--- review2.js
+++ review2.js
@@ -1,13 +1,27 @@
-const output = doubleMe(5);
+// const output = doubleMe(5);
 
-function doubleMe(num) {
-  return num * 2;
-}
+// function doubleMe(num) {
+//   return num * 2;
+// }
 
-// hoisted
-console.log(output);
+// function tripleMe(num) {
+//   return num * 3;
+// }
 
-// normal
-console.log(doubleMe(6));
-console.log(doubleMe(69));
-console.log(doubleMe(100));
+// // hoisted
+// console.log(output);
+
+// // normal
+// console.log(doubleMe(69));
+// console.log(doubleMe(100));
+
+// // overloading
+// console.log(tripleMe(doubleMe(6)));
+
+const object = {
+  log: () => {
+    return console.log('Hello World!!');
+  },
+};
+
+object.log();
