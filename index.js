620ca17ad9d55eff4e2d038ac5e8f7f0c691308a
Jose Perez Jr <zenlaws02@gmail.com>
2024-10-05 03:31:54 +0800

    fix: updated the code to just destructure the temp_c and condition from weatherData

--- index.js
+++ index.js
@@ -1,22 +1,25 @@
 const temp = document.querySelector('#weather-temp');
-const condition = document.querySelector('#weather-condition');
+const weather_condition = document.querySelector('#weather-condition');
 const icon = document.querySelector('#weather-icon');
 
 async function start() {
   const weatherPromise = await fetch(
-    'https://api.weatherapi.com/v1/current.json?q=14.682420719185082%2C121.12798023158746&key=0646a2235f34450f91e174616240410'
+    'https://api.weatherapi.com/v1/current.json?q=14.682413578492366%2C121.12798895707499&key=0646a2235f34450f91e174616240410'
   );
   const weatherData = await weatherPromise.json();
   console.log(weatherData);
   console.log(weatherData.current.condition.text);
   console.log(`${weatherData.current.temp_c}°C`);
 
-  const { temp_c, condition } = weatherData.current; // destructure from object
+  // temp.textContent = weatherData.current.temp_c;
+  // weather_condition.textContent = `${weatherData.current.condition.text}`;
+  // icon.src = `${weatherData.current.condition.icon}`;
+
+  const { temp_c, condition } = weatherData.current;
 
   temp.textContent = temp_c;
-  condition.textContent = condition.text;
+  weather_condition.textContent = condition.text;
   icon.src = condition.icon;
-
 }
 
 start();
