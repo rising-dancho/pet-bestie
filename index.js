7e2519d6a2d42e0fbf95beefc86fb4fabb3ab8c1
Jose Perez Jr <zenlaws02@gmail.com>
2024-10-07 05:18:11 +0800

    feat: in progress, updating the filter buttons in the pet area

--- index.js
+++ index.js
@@ -4,7 +4,6 @@ const weather_condition = document.querySelector('#weather-condition');
 const icon = document.querySelector('#weather-icon');
 const if_error = document.querySelector('#if-error');
 
-
 async function weather() {
   try {
     const weatherPromise = await fetch(
@@ -45,7 +44,6 @@ async function weather() {
     // Optional: log the full error for debugging
     console.error('Fetch error:', error);
   }
-
 }
 
 weather();
@@ -116,10 +114,11 @@ allFilterButtons.forEach((button) => {
   button.addEventListener('click', handleButtonClick);
 });
 
-function handleButtonClick() {
+function handleButtonClick(e) {
   // remove active class from any and all buttons
   allFilterButtons.forEach((button) => button.classList.remove('active'));
   // add active class to the specific button that just got clicked
+  e.target.classList.add('active');
   // actually filter the pets down below
 }
 
