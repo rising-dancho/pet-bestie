41bb7629eb6718dc02ab6aee3ff0c515bdc4d17c
adfinem <43742265+rising-dancho@users.noreply.github.com>
2024-10-05 17:12:33 -0700

    Pets area (#10)
    * feat: in progress, updating the pet cards
    
    * feat: in progress, updated the photo section of the cards and added fallback image
    
    * feat: in progress, updated the photo section of the cards and added fallback image
    
    * feat: in progress, updated the photo section of the cards and added fallback image
    
    * feat: in progress, updated the photo section of the cards and added fallback image

--- index.js
+++ index.js
@@ -7,11 +7,7 @@ const wrapper = document.createDocumentFragment();
 const temp = document.querySelector('#weather-temp');
 const weather_condition = document.querySelector('#weather-condition');
 const icon = document.querySelector('#weather-icon');
-
-// location
-// const location_name = document.querySelector('#location-name');
-// const location_region = document.querySelector('#location-region');
-// const location_country = document.querySelector('#location-country');
+const if_error = document.querySelector('#if-error');
 
 async function start() {
   const weatherPromise = await fetch(
@@ -22,26 +18,17 @@ async function start() {
   console.log(weatherData.current.condition.text);
   console.log(`${weatherData.current.temp_c}°C`);
 
-  // temp.textContent = weatherData.current.temp_c;
-  // weather_condition.textContent = `${weatherData.current.condition.text}`;
-  // icon.src = `${weatherData.current.condition.icon}`;
-
   // WEATHER
   const { temp_c, condition } = weatherData.current;
+
+  // FALLBACK: in case of weather api error
+  if (!temp_c || !condition.text || !condition.icon)
+    if_error =
+      "[Whoops! Can't fetch weather data right now. Please try again later.. ✌️]";
 
   temp.textContent = temp_c;
   weather_condition.textContent = condition.text;
   icon.src = condition.icon;
-
-  // LOCATION
-  // const { name, region, country } = weatherData.location;
-  // console.log(name);
-  // console.log(region);
-  // console.log(country);
-
-  // location_name.textContent = name;
-  // location_region.textContent = region;
-  // location_country.textContent = country;
 }
 
 start();
@@ -72,9 +59,10 @@ async function petsArea() {
     clone.querySelector('h3').textContent = pet.name;
     clone.querySelector('.pet-description').textContent = pet.description;
     clone.querySelector('.pet-age').textContent = calculateAge(pet.birthYear);
-    clone.querySelector('.pet-card-photo img').src = pet.photo
-      ? pet.photo
-      : './images/fallback.jpg';
+
+    if (!pet.photo) pet.photo = './images/fallback.jpg';
+
+    clone.querySelector('.pet-card-photo img').src = pet.photo;
     clone.querySelector(
       '.pet-card-photo img'
     ).alt = `A ${pet.species} named ${pet.name}`;
