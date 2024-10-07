fc2a3cdeea7a62e6647641561de042b5afb2e330
adfinem <43742265+rising-dancho@users.noreply.github.com>
2024-10-07 09:56:33 -0700

    Pets area (#18)
    * feat: in progress, updating the pet cards
    
    * feat: in progress, updated the photo section of the cards and added fallback image
    
    * feat: in progress, updated the photo section of the cards and added fallback image
    
    * feat: in progress, updated the photo section of the cards and added fallback image
    
    * feat: in progress, updated the photo section of the cards and added fallback image
    
    * feat: in progress, updated the photo section of the cards and added fallback image
    
    * fix: added a try catch for the fetch weather api and included fall back error message. also adjusted the media queries
    
    * feat: in progress, updating the filter buttons in the pet area
    
    * fix: removed the extra °C for the weather temperature
    
    * fix: updated the fallback error message
    
    * fix: updated the fallback error message

--- index.js
+++ index.js
@@ -29,8 +29,8 @@ async function weather() {
       if_error.style.fontweight = '900';
       if_error.style.marginTop = '12px';
 
-      if_error.textContent =
-        'Whoops! The request took too long to respond. Please try again later.. ✌️';
+      if_error.textContent = "Whoops! We can't seem to get the weather forecast right now. Please try again later.. ✌️";
+
     } else {
       if_error.style.display = 'block';
       if_error.style.color = '#332700';
